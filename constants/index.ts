509fa4769465cabd28cf7bd2e859b3a7e72332d0
tintheanh <tintheanh@gmail.com>
2020-08-19 17:20:52 -0700

    remove some unneccessary packages

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
-import { getSystemName, hasNotch } from 'react-native-device-info';
+import { NativeModules, Platform } from 'react-native';
 
 import Colors from './Colors';
 import Layout from './Layout';
@@ -8,7 +8,6 @@ export const oneMonth = 2.628e9;
 export const oneYear = 3.154e10;
 
 export const tokenForTag = '\u200B';
-// export const tokenForTag = '\u01AA';
 export const separatorForTag = '\u01B1';
 
 export const pendingPostID = 'pending-post-69';
@@ -18,9 +17,321 @@ export const pendingDeleteCommentFlag = '--pending-delete-comment';
 export const pendingReplyID = 'pending-reply-69';
 export const pendingDeleteReplyFlag = '--pending-delete-reply';
 
-const deviceOS = getSystemName();
-export const bottomTabHeight =
-  deviceOS === 'iOS' || deviceOS === 'iPhone OS' ? (hasNotch() ? 79 : 50) : 50;
-
 export { Colors, Layout };
 export * from './Icons';
+
+const { RNDeviceInfo } = NativeModules;
+
+const devicesWithNotch = [
+  {
+    brand: 'Apple',
+    model: 'iPhone 11',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone 11 Pro',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone 11 Pro Max',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone X',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone XS',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone XS Max',
+  },
+  {
+    brand: 'Apple',
+    model: 'iPhone XR',
+  },
+  {
+    brand: 'Asus',
+    model: 'ZenFone 5',
+  },
+  {
+    brand: 'Asus',
+    model: 'ZenFone 5z',
+  },
+  {
+    brand: 'google',
+    model: 'Pixel 3 XL',
+  },
+  {
+    brand: 'Huawei',
+    model: 'P20',
+  },
+  {
+    brand: 'Huawei',
+    model: 'P20 Plus',
+  },
+  {
+    brand: 'Huawei',
+    model: 'P20 Lite',
+  },
+  {
+    brand: 'Huawei',
+    model: 'ANE-LX1',
+  },
+  {
+    brand: 'Huawei',
+    model: 'INE-LX1',
+  },
+  {
+    brand: 'Huawei',
+    model: 'POT-LX1',
+  },
+  {
+    brand: 'Huawei',
+    model: 'Honor 10',
+  },
+  {
+    brand: 'Huawei',
+    model: 'Mate 20 Lite',
+  },
+  {
+    brand: 'Huawei',
+    model: 'Mate 20 Pro',
+  },
+  {
+    brand: 'Huawei',
+    model: 'ELE-L29', // P30
+  },
+  {
+    brand: 'Huawei',
+    model: 'P30 Lite',
+  },
+  {
+    brand: 'Huawei',
+    model: 'P30 Pro',
+  },
+  {
+    brand: 'Huawei',
+    model: 'Nova 3',
+  },
+  {
+    brand: 'Huawei',
+    model: 'Nova 3i',
+  },
+  {
+    brand: 'Leagoo',
+    model: 'S9',
+  },
+  {
+    brand: 'LG',
+    model: 'G7',
+  },
+  {
+    brand: 'LG',
+    model: 'G7 ThinQ',
+  },
+  {
+    brand: 'LG',
+    model: 'G7+ ThinQ',
+  },
+  {
+    brand: 'LG',
+    model: 'LM-Q910', //G7 One
+  },
+  {
+    brand: 'LG',
+    model: 'LM-G710', //G7 ThinQ
+  },
+  {
+    brand: 'LG',
+    model: 'LM-V405', //V40 ThinQ
+  },
+  {
+    brand: 'Motorola',
+    model: 'Moto g7 Play',
+  },
+  {
+    brand: 'Motorola',
+    model: 'Moto g7 Power',
+  },
+  {
+    brand: 'Motorola',
+    model: 'One',
+  },
+  {
+    brand: 'Motorola',
+    model: 'Motorola One Vision',
+  },
+  {
+    brand: 'Nokia',
+    model: '5.1 Plus',
+  },
+  {
+    brand: 'Nokia',
+    model: 'Nokia 6.1 Plus',
+  },
+  {
+    brand: 'Nokia',
+    model: '7.1',
+  },
+  {
+    brand: 'Nokia',
+    model: '8.1',
+  },
+  {
+    brand: 'OnePlus',
+    model: '6',
+  },
+  {
+    brand: 'OnePlus',
+    model: 'A6003',
+  },
+  {
+    brand: 'ONEPLUS',
+    model: 'A6000',
+  },
+  {
+    brand: 'OnePlus',
+    model: 'OnePlus A6003',
+  },
+  {
+    brand: 'OnePlus',
+    model: 'ONEPLUS A6010',
+  },
+  {
+    brand: 'OnePlus',
+    model: 'ONEPLUS A6013',
+  },
+  {
+    brand: 'OnePlus',
+    model: 'ONEPLUS A6000',
+  },
+  {
+    brand: 'Oppo',
+    model: 'R15',
+  },
+  {
+    brand: 'Oppo',
+    model: 'R15 Pro',
+  },
+  {
+    brand: 'Oppo',
+    model: 'F7',
+  },
+  {
+    brand: 'Oukitel',
+    model: 'U18',
+  },
+  {
+    brand: 'Sharp',
+    model: 'Aquos S3',
+  },
+  {
+    brand: 'Vivo',
+    model: 'V9',
+  },
+  {
+    brand: 'Vivo',
+    model: 'X21',
+  },
+  {
+    brand: 'Vivo',
+    model: 'X21 UD',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'MI 8',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'MI 8 Explorer Edition',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'MI 8 SE',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'MI 8 UD',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'MI 8 Lite',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'Mi 9',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'POCO F1',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'POCOPHONE F1',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'Redmi 6 Pro',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'Redmi Note 7',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'Redmi Note 8',
+  },
+  {
+    brand: 'xiaomi',
+    model: 'Mi A2 Lite',
+  },
+  {
+    brand: 'Blackview',
+    model: 'A30',
+  },
+];
+
+function getBrand() {
+  let brand = '';
+  if (
+    Platform.OS === 'android' ||
+    Platform.OS === 'ios' ||
+    Platform.OS === 'windows'
+  ) {
+    brand = RNDeviceInfo.brand;
+  } else {
+    brand = 'unknown';
+  }
+  return brand;
+}
+
+function getModel() {
+  let model = '';
+  if (
+    Platform.OS === 'ios' ||
+    Platform.OS === 'android' ||
+    Platform.OS === 'windows'
+  ) {
+    model = RNDeviceInfo.model;
+  } else {
+    model = 'unknown';
+  }
+  return model;
+}
+
+function hasNotch() {
+  let notch = false;
+  const brand = getBrand();
+  const model = getModel();
+  notch =
+    devicesWithNotch.findIndex(
+      (item) =>
+        item.brand.toLowerCase() === brand.toLowerCase() &&
+        item.model.toLowerCase() === model.toLowerCase(),
+    ) !== -1;
+  return notch;
+}
+
+export const bottomTabHeight = hasNotch() ? 79 : 50;
--- utils/functions.ts
+++ utils/functions.ts
@@ -1,9 +1,7 @@
-import moment from 'moment';
 import { Alert, Linking } from 'react-native';
 import { v4 as uuidv4 } from 'uuid';
 import { URL } from 'react-native-url-polyfill';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
-import URI from 'urijs';
 import {
   fsDB,
   auth,
@@ -99,7 +97,6 @@ const checkPlaceListChanged = (
  */
 const randomlyThrowError = () => {
   const percentage = Math.round(Math.random() * 99) + 1;
-  console.log(percentage);
   if (percentage > 50) {
     throw new Error('Fake error.');
   }
@@ -146,56 +143,52 @@ const isUsernameValid = (username: string) => {
   return regex.test(username);
 };
 
-const convertTime = (unix: number) => {
-  const relative = moment.unix(Math.floor(unix / 1000)).fromNow();
-
-  // console.log(relative);
-
-  if (relative === 'a few seconds ago') {
-    return 'Just now';
+const convertTime = (unixTime: number) => {
+  const now = new Date().getTime();
+  if (unixTime <= 0 || unixTime > now) {
+    return '';
   }
 
-  if (relative.includes('a minute')) {
-    return '1m';
+  // Calculate difference
+  const difference = Math.abs(unixTime / 1000 - now / 1000);
+
+  let unitOfTime = '';
+  let time = 0;
+  // Calculate time unit
+  if (difference / (60 * 60 * 24 * 365) > 1) {
+    // Years
+    unitOfTime = 'y';
+    time = Math.floor(difference / (60 * 60 * 24 * 365));
+  } else if (difference / (60 * 60 * 24 * 45) > 1) {
+    // Months
+    unitOfTime = 'mo';
+    time = Math.floor(difference / (60 * 60 * 24 * 45));
+  } else if (difference / (60 * 60 * 24) > 1) {
+    // Days
+    unitOfTime = 'd';
+    time = Math.floor(difference / (60 * 60 * 24));
+  } else if (difference / (60 * 60) > 1) {
+    // Hours
+    unitOfTime = 'h';
+    time = Math.floor(difference / (60 * 60));
+  } else if (difference / 60 > 1) {
+    // Minutes
+    unitOfTime = 'mi';
+    time = Math.floor(difference / 60);
+  } else {
+    // Seconds
+    unitOfTime = 'Just now';
   }
 
-  if (relative.includes('an hour')) {
-    return '1h';
+  // Return time from now data
+  if (time === 0) {
+    return unitOfTime;
   }
-
-  if (relative.includes('a day')) {
-    return '1d';
-  }
-
-  if (relative.includes('a month')) {
-    return '1mo';
-  }
-
-  if (relative.includes('a year')) {
-    return '1y';
-  }
-
-  const split = relative.split(' ');
-  if (relative.includes('minutes')) {
-    return `${split[0]}m`;
-  }
-
-  if (relative.includes('hours')) {
-    return `${split[0]}hr`;
-  }
-
-  if (relative.includes('days')) {
-    return `${split[0]}d`;
-  }
-
-  if (relative.includes('months')) {
-    return `${split[0]}mo`;
-  }
-  return `${split[0]}y`;
+  return `${time}${unitOfTime}`;
 };
 
 const getCurrentUnixTime = () => {
-  return moment().valueOf();
+  return new Date().getTime();
 };
 
 const delay = (ms: number) => {
@@ -211,7 +204,7 @@ const convertNumber = (num: number) => {
 
 const checkURL = (url: string) => {
   try {
-    new URL(url);
+    const _ = new URL(url);
   } catch (_) {
     return false;
   }
